6464094968e00a87aa291a95a075d9251b05023b
dinellipaulo <pgsdinelli@gmail.com>
2024-09-03 15:14:17 -0300

    Created attributes on class GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,11 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int playerCoordX[] = new int[GAME_UNITS];
+    final int playerCoordY[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int foodEaten;
+    int foodCoordX;
+    int foodCoordY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     public GamePanel(){
         this.add(new GamePanel());
